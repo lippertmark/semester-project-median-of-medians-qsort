10412ab5d11d49ffa80c563a7454ff59d7c4e5c9
lippertmark <lippert.mark.d@gmail.com>
2022-05-16 19:45:04 +0300

    testing successfully

--- tests/test_sort.py
+++ tests/test_sort.py
@@ -1,17 +1,20 @@
 import unittest
 import random
+from src.quick_sort import Sort
 
 
 class TestSort(unittest.TestCase):
-    @staticmethod
-    def sort_list(lst):
-        return sorted(lst)
+
     def test_sort_empty_list(self):
-        self.assertEqual([], TestSort.sort_list([]))
+        a = []
+        Sort(a)
+        self.assertEqual([], a)
 
     def test_sort(self):
         j = 10
-        while j<=100000:
+        while j <= 100000:
             randomlist = random.sample(range(10, 30), 5)
-            self.assertEqual(sorted(randomlist), TestSort.sort_list(randomlist))
-            j*=10
+            unsorted = randomlist.copy()
+            Sort(randomlist)
+            self.assertEqual(sorted(unsorted), randomlist)
+            j *= 10
