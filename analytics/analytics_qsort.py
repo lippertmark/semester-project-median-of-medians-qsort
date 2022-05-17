d336a3dc07158b7809f110e51a036947ad92009e
ttalova <talova0304@gmail.com>
2022-05-17 21:58:44 +0300

    commit

---
+++ analytics/analytics_qsort.py
@@ -0,0 +1,21 @@
+from src.quick_sort import Sort
+from src.quick_sort import KthSmallest
+from src.quick_sort import partition
+from src.quick_sort import findMedian
+import time
+import numpy
+
+def check_Sort():
+    for count_of_elems in range(100, 10**5, 100):
+        mas = list(numpy.random.randint(0, 10 ** 6, count_of_elems))
+        start = time.time()
+        Sort(mas)
+        finish = time.time() - start
+        with open('analytics_data/analytic_Sort.txt', 'a') as file:
+            file.write(f'{count_of_elems} {finish}\n')
+
+def check_KthSmallest():
+    pass
+
+if __name__ == '__main__':
+    check_Sort()
